669be4741cc41e161dd9b6c31426e0b095558359
juandtr <juandtr1999@hmail.com>
2021-08-11 15:31:32 -0500

    PI-Countries-FT-15A prueba3

--- api/index.js
+++ api/index.js
@@ -1,28 +1,26 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const fetch = require("node-fetch");
+const { conn, Country } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((r) => r.json())
+      .then((r) => {
+        r.forEach((el) => {
+          let country = {
+            id: el.alpha3Code,
+            name: el.name === "Åland Islands" ? "Aland Islands" : el.name,
+            flag: el.flag,
+            continent: el.region,
+            area: el.area,
+            population: el.population,
+          };
+          Country.create(country);
+        });
+      })
+      .catch((err) => console.log(err));
   });
 });
